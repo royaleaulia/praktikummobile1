a3f72bdd36d4d11d645d44b405d7ec16a57c2417
royaleaulia <101610494+royaleaulia@users.noreply.github.com>
2022-03-22 13:46:17 +0800

    Update PRAK102.kt

--- PRAK102.kt
+++ PRAK102.kt
@@ -1,28 +1,7 @@
-import java.util.Scanner
-
 fun main(){
-
-
-    // mengambil input
-    val reader = Scanner(System.`in`)
-    print("Waktu Sekarang: ")
-    val waktu = readLine()
-
-    print("Nama Anda: ")
-    val nama = readLine()
-    print("Umur Anda: ")
-    var umur = Integer.valueOf(readLine())
-    print("Suhu Tubuh Anda: ")
-    var suhutubuh:Float = reader.nextFloat()
-    // menampilkan output
-
-    println("Selamat $waktu, $nama.")
-    println("Umur Anda $umur Tahun")
-    if(suhutubuh >= 35 && suhutubuh <= 37 ){
-        println("Suhu tubuh anda $suhutubuh derajat Celcius.")
-    }
-    else{
-        println("Suhu Anda aneh, Anda bukan manusia.")
-    }
+//
+    print("Nilai x = ")
+    var nilai = Integer.valueOf(readLine())
+    var fx = Integer.valueOf(2*(nilai*nilai)+(5*nilai)-8)
+    print("$fx")
 }
-
